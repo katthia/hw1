3f3f5903bb6cf5b04582e48dfdf493079bcee2e1
katthia <165447984+katthia@users.noreply.github.com>
2025-02-09 22:09:07 -0600

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,11 @@
+/*
+    Author: Kathia Graciano
+    Date Created: February 6, 2025
+    Date last edited: February 9, 2025
+    Version: 1.0
+    Description: JavaScript for hw 1
+*/
+/*/ From profjake's hw3 */
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
